fa12685c908051493ee2d45642093d2f85849a4b
Braxton Frederick <bfrederick@constantcontact.com>
2016-06-25 15:54:36 -0400

    Added aarons face

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,19 +1,32 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
     './src/index.jsx'
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel'
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot!babel'
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style', 'css',
+        ]
+      }
+    ]
   },
   resolve: {
+    alias: {
+      'react': path.join(__dirname, 'node_modules', 'react')
+    },
     extensions: ['', '.js', '.jsx']
   },
   output: {
